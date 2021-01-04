fa54d0c236d1c74848c3c5f67a413f4f87f50237
Practical IT <code@thepracticalit.com>
2021-01-03 20:58:39 -0500

    Initial commit for file neat

---
+++ src/views/index.js
@@ -0,0 +1,13 @@
+export { default as AboutSideCover } from './AboutSideCover';
+export { default as Account } from './Account';
+export { default as BlogArticle } from './BlogArticle';
+export { default as ContactPage } from './ContactPage';   
+export { default as CompanyTerms } from './CompanyTerms';
+export { default as PasswordResetSimple } from './PasswordResetSimple';
+export { default as Pricing } from './Pricing';
+export { default as SigninSimple } from './SigninSimple';
+export { default as SignupSimple } from './SignupSimple';
+export { default as Thankyou } from './Thankyou';
+export { default as NotFound } from './NotFound';
+export { default as NotFoundCover } from './NotFoundCover';
+export { default as WebBasic } from './WebBasic';
