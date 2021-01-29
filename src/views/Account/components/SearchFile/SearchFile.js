43d3fda6991ef80ee3c9d5aa28761ab5791d61c3
Melak <melakubetew@gmail.com>
2021-01-29 17:44:59 -0500

    create communication with server

--- src/views/Account/components/SearchFile/SearchFile.js
+++ src/views/Account/components/SearchFile/SearchFile.js
@@ -10,7 +10,6 @@ import {
   Button,
   Divider,
 } from '@material-ui/core';
-
 const useStyles = makeStyles(theme => ({
   root: {},
   inputTitle: {
@@ -20,6 +19,22 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const General = props => {
+  const [search, setSearch] =React.useState("");
+  const handlesubmit =async (e)=>{
+   e.preventDefault();
+   try {
+     const searchres =await fetch(
+   `${process.env.REACT_APP_BACKEND_SERVER}${process.env.REACT_APP_SEARCH_FILE}/?comp_id=${search}`
+  // 'http://file.solutionladder.com/ajax.php?action=serch-file&comp_id=CUSTOMER_ID_HERE'
+     );
+   
+     const res =await searchres.json();
+     props.callBackMe(res);
+   } catch (error){
+     console.log(error.message)
+   }
+   };
+  
   const { className, ...rest } = props;
   const classes = useStyles();
 
@@ -30,35 +45,49 @@ const General = props => {
 
   return (
     <div className={clsx(classes.root, className)} {...rest}>
+      
       <Grid container spacing={isMd ? 4 : 2}>
-        <Grid item xs={12}>
+       
+       <Grid item xs={12}>
           <Typography
             variant="subtitle1"
             color="textPrimary"
             className={classes.inputTitle}
           >
-          Customer Name
+          Customer Id
           </Typography>
+          </Grid>
+           
+          <form onSubmit={handlesubmit}>
+            <Grid item xs={12}>
           <TextField
-            placeholder="Customer Name"
+            placeholder="Customer Id"
             variant="outlined"
-            size="medium"
+            size="large"
             name="fullname"
             fullWidth
             type="text"
+            value={search}
+            onChange={(e)=>{setSearch(e.target.value);
+              // console.log(e.target.value);
+              }}
+            
           />
         </Grid>
         
-        </Grid>
+        
         <Grid item container justify="flex-start" xs={12}>
           <Button
             variant="contained"
             type="submit"
             color="primary"
-            size="large"
+            size="small"
           >
            Search
           </Button>
+          
+        </Grid>
+        </form>
         </Grid>
     </div>
   );
